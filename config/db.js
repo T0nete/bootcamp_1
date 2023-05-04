a1bfcf075c20b70d623c6690572a240aa987db1f
T0nete <tonbarramon@gmail.com>
2023-05-04 23:22:05 +0200

    connect database + docker-compose file

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://mongo:27017/bootcamp_mongo'
+// const url = 'mongodb://db:27017/' // Docker
+const url = 'mongodb://localhost:27017/' // Local
 
 const connectDB = () => {
-  mongoose.connect(url, () =>
-    console.log('Connected to DB'))
+  mongoose.connect(
+    url,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connection succesfully established with Mongo'))
 }
 
 module.exports = connectDB
